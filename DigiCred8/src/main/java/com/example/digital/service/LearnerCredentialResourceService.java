69f98804d2e60914849af8149ac6c49a3e600537
vijaykannuri <kvijay@triconinfotech.com>
2018-09-18 13:41:35 +0530

    changing learnercredentialResouce response

--- DigiCred8/src/main/java/com/example/digital/service/LearnerCredentialResourceService.java
+++ DigiCred8/src/main/java/com/example/digital/service/LearnerCredentialResourceService.java
@@ -2,11 +2,12 @@ package com.example.digital.service;
 
 import com.example.digital.entity.LearnerCredentialResource;
 import com.example.digital.entity.LearnerCredentialResourceRequest;
+import com.example.digital.entity.LearnerCredentialResourceResponse;
 
 import java.util.List;
 
 public interface LearnerCredentialResourceService {
 
     LearnerCredentialResource save(LearnerCredentialResourceRequest learnerCredentialResourceRequest) throws Exception;
-    List<LearnerCredentialResource> getLeranerCredentialResourceByUserId(Long userId);
+    List<LearnerCredentialResourceResponse> getLeranerCredentialResourceByUserId(Long userId);
 }
